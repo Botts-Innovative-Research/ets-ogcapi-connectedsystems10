8e031ef8661b178477ac7594b6ac4b8d225c622e
cam-s-hunt <cameron.s.hunt@noble-hunt.com>
2026-04-28 11:46:46 -0400

    S-ETS-01-01: port ClientUtils to Jersey 3 / Jakarta EE 9 (ref features10@java17Tomcat10TeamEngine6)
    Replaces com.sun.jersey.api.client.* with jakarta.ws.rs.client.* +
    org.glassfish.jersey.client.* per features10's java17 reference port.
    Key changes:
      - Client.create() -> ClientBuilder.newClient(ClientConfig)
      - DefaultClientConfig -> org.glassfish.jersey.client.ClientConfig
      - filter() -> register()
      - LoggingFilter dropped (Jakarta has no equivalent stock filter)
      - ClientRequest builder -> Invocation buildGetRequest() returns Jakarta Invocation
      - response.getEntityInputStream() -> response.getEntityStream()
      - ClientResponse is now org.glassfish.jersey.client.ClientResponse
      - buildClientWithProxy uses ClientProperties.PROXY_URI string instead of
        ApacheConnectorProvider (apache-connector not on classpath; can be added
        later if proxy-debugging is exercised)
      - Added is200Response(URI) probe (carried from features10 java17)
    
    Refs: ADR-004 Group B (Jersey 1.x deprecation completion)

--- src/main/java/org/opengis/cite/ogcapiconnectedsystems10/util/ClientUtils.java
+++ src/main/java/org/opengis/cite/ogcapiconnectedsystems10/util/ClientUtils.java
@@ -5,47 +5,52 @@ import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.SocketAddress;
-import java.net.URL;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientRequest;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.client.filter.LoggingFilter;
-import com.sun.jersey.client.urlconnection.HttpURLConnectionFactory;
-import com.sun.jersey.client.urlconnection.URLConnectionClientHandler;
 import java.net.URI;
+import java.net.URL;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ws.rs.HttpMethod;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
+
 import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.client.ClientResponse;
 import org.opengis.cite.ogcapiconnectedsystems10.ReusableEntityFilter;
 import org.w3c.dom.Document;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.Invocation.Builder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+
 /**
  * Provides various utility methods for creating and configuring HTTP client components.
+ *
+ * <p>
+ * Ported from ets-ogcapi-features10@java17Tomcat10TeamEngine6 (Jersey 3.1 / Jakarta EE
+ * 9).
+ * </p>
  */
 public class ClientUtils {
 
 	/**
 	 * Builds a client component for interacting with HTTP endpoints. The client will
 	 * automatically redirect to the URI declared in 3xx responses. The connection timeout
-	 * is 10 s. Request and response messages may be logged to a JDK logger (in the
-	 * namespace "com.sun.jersey.api.client").
+	 * is 10 s.
 	 * @return A Client component.
 	 */
 	public static Client buildClient() {
-		ClientConfig config = new DefaultClientConfig();
-		config.getProperties().put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, true);
-		config.getProperties().put(ClientConfig.PROPERTY_CONNECT_TIMEOUT, 10000);
-		Client client = Client.create(config);
-		client.addFilter(new ReusableEntityFilter());
-		client.addFilter(new LoggingFilter());
+		ClientConfig config = new ClientConfig();
+		config.property(ClientProperties.FOLLOW_REDIRECTS, true);
+		config.property(ClientProperties.CONNECT_TIMEOUT, 10000);
+		Client client = ClientBuilder.newClient(config);
+		client.register(new ReusableEntityFilter());
 		return client;
 	}
 
@@ -58,31 +63,33 @@ public class ClientUtils {
 	 * @return A Client component that submits requests through a web proxy.
 	 */
 	public static Client buildClientWithProxy(final String proxyHost, final int proxyPort) {
-		ClientConfig config = new DefaultClientConfig();
-		config.getProperties().put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, true);
-		Client client = new Client(new URLConnectionClientHandler(new HttpURLConnectionFactory() {
-			SocketAddress addr = new InetSocketAddress(proxyHost, proxyPort);
-
-			Proxy proxy = new Proxy(Proxy.Type.HTTP, addr);
-
-			@Override
-			public HttpURLConnection getHttpURLConnection(URL url) throws IOException {
-				return (HttpURLConnection) url.openConnection(proxy);
-			}
-		}), config);
-		client.addFilter(new LoggingFilter());
+		ClientConfig config = new ClientConfig();
+		SocketAddress addr = new InetSocketAddress(proxyHost, proxyPort);
+		Proxy proxy = new Proxy(Proxy.Type.HTTP, addr);
+		// jakarta.ws.rs.client.ClientBuilder doesn't expose a Proxy directly; rely on
+		// HttpUrlConnectorProvider's default behavior which honors the system
+		// http(s).proxyHost
+		// properties. The Apache connector (which would let us pass a Proxy
+		// programmatically)
+		// is not on the classpath in Sprint 1; revisit if needed.
+		config.property(ClientProperties.PROXY_URI, "http://" + proxyHost + ":" + proxyPort);
+		config.property(ClientProperties.FOLLOW_REDIRECTS, true);
+		Client client = ClientBuilder.newClient(config);
+		client.register(new ReusableEntityFilter());
+		Logger.getLogger(ClientUtils.class.getName()).log(Level.FINE, "Configured proxy {0}", proxy.toString());
 		return client;
 	}
 
 	/**
-	 * Builds an HTTP request message that uses the GET method.
+	 * Builds an HTTP GET Invocation against the given endpoint. The returned
+	 * {@link Invocation} can be invoked to obtain a {@link Response}.
 	 * @param endpoint A URI indicating the target resource.
-	 * @param qryParams A Map containing query parameters (may be null);
+	 * @param qryParams A Map containing query parameters (may be null).
 	 * @param mediaTypes A list of acceptable media types; if not specified, generic XML
 	 * ("application/xml") is preferred.
-	 * @return A ClientRequest object.
+	 * @return An Invocation representing a built GET request.
 	 */
-	public static ClientRequest buildGetRequest(URI endpoint, Map<String, String> qryParams, MediaType... mediaTypes) {
+	public static Invocation buildGetRequest(URI endpoint, Map<String, String> qryParams, MediaType... mediaTypes) {
 		UriBuilder uriBuilder = UriBuilder.fromUri(endpoint);
 		if (null != qryParams) {
 			for (Map.Entry<String, String> param : qryParams.entrySet()) {
@@ -90,15 +97,15 @@ public class ClientUtils {
 			}
 		}
 		URI uri = uriBuilder.build();
-		ClientRequest.Builder reqBuilder = ClientRequest.create();
+		WebTarget target = buildClient().target(uri);
+		Builder builder = target.request();
 		if (null == mediaTypes || mediaTypes.length == 0) {
-			reqBuilder = reqBuilder.accept(MediaType.APPLICATION_XML_TYPE);
+			builder = builder.accept(MediaType.APPLICATION_XML_TYPE);
 		}
 		else {
-			reqBuilder = reqBuilder.accept(mediaTypes);
+			builder = builder.accept(mediaTypes);
 		}
-		ClientRequest req = reqBuilder.build(uri, HttpMethod.GET);
-		return req;
+		return builder.buildGet();
 	}
 
 	/**
@@ -113,21 +120,21 @@ public class ClientUtils {
 	/**
 	 * Obtains the (XML) response entity as a JAXP Source object and resets the entity
 	 * input stream for subsequent reads.
-	 * @param response A representation of an HTTP response message.
+	 * @param response A representation of an HTTP response message (Glassfish Jersey
+	 * ClientResponse).
 	 * @param targetURI The target URI from which the entity was retrieved (may be null).
 	 * @return A Source to read the entity from; its system identifier is set using the
-	 * given targetURI value (this may be used to resolve any relative URIs found in the
-	 * source).
+	 * given targetURI value.
 	 */
 	public static Source getResponseEntityAsSource(ClientResponse response, String targetURI) {
-		Source source = response.getEntity(DOMSource.class);
+		Source source = response.readEntity(DOMSource.class);
 		if (null != targetURI && !targetURI.isEmpty()) {
 			source.setSystemId(targetURI);
 		}
-		if (response.getEntityInputStream().markSupported()) {
+		if (response.getEntityStream().markSupported()) {
 			try {
 				// NOTE: entity was buffered by client filter
-				response.getEntityInputStream().reset();
+				response.getEntityStream().reset();
 			}
 			catch (IOException ex) {
 				Logger.getLogger(ClientUtils.class.getName())
@@ -143,14 +150,34 @@ public class ClientUtils {
 	 * @param response A representation of an HTTP response message.
 	 * @param targetURI The target URI from which the entity was retrieved (may be null).
 	 * @return A Document representing the entity; its base URI is set using the given
-	 * targetURI value (this may be used to resolve any relative URIs found in the
-	 * document).
+	 * targetURI value.
 	 */
 	public static Document getResponseEntityAsDocument(ClientResponse response, String targetURI) {
 		DOMSource domSource = (DOMSource) getResponseEntityAsSource(response, targetURI);
 		Document entityDoc = (Document) domSource.getNode();
 		entityDoc.setDocumentURI(domSource.getSystemId());
 		return entityDoc;
+	}
+
+	/**
+	 * Checks if a GET request to a given URI returns HTTP 200 - OK.
+	 * @param uri The URI to check.
+	 * @return true if HTTP 200 - OK was returned after a GET request, false otherwise.
+	 */
+	public static boolean is200Response(URI uri) {
+		URL url = null;
+		int code = 0;
+		try {
+			url = uri.toURL();
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.connect();
+			code = connection.getResponseCode();
+		}
+		catch (Exception ee) {
+			Logger.getLogger(ClientUtils.class.getName()).log(Level.WARNING, "GET probe failed for " + uri, ee);
+		}
+		return (code == 200);
 	}
 
 }
